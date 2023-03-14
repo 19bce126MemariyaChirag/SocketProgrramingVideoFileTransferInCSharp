6f11464c540e4667c7e6bb4b18ec5fef1fb276ec
19bce126MemariyaChirag <100746831+19bce126MemariyaChirag@users.noreply.github.com>
2023-03-14 17:32:04 +0530

    Update Client.cs

--- Client.cs
+++ Client.cs
@@ -10,44 +10,33 @@ namespace MyExampleNamesapce
 {
     public class MySocketClient
     {
-        static async Task Main(string[] args)
+        static void  Main(string[] args)
         {
-            IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync("localhost");
-            IPAddress ipAddress = ipHostInfo.AddressList[0];
-            // IPEndPoint ipEndPoint = new(new IPAddress(new byte[] { 172, 16, 0, 205 }), 8080);
-            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 8080);
+            
 
+            // IPHostEntry ipHostInfo = Dns.GetHostEntry("localhost");
+            // IPAddress ipAddress = ipHostInfo.AddressList[0];
+            // IPEndPoint ipEndPoint = new (ipAddress, 8080);
+           IPEndPoint ipEndPoint = new IPEndPoint(new IPAddress(new byte[]{172,16,0,189}), 8080);
 
             using Socket client = new(
                 ipEndPoint.AddressFamily,
                 SocketType.Stream,
                 ProtocolType.Tcp);
 
-            await client.ConnectAsync(ipEndPoint);
+       
             Console.WriteLine("Client is connected!");
-            while (true)
-            {
-                // Send message.
-                Console.Write("Enter Message : ");
-                String str = Console.ReadLine();
-                var messageBytes = Encoding.UTF8.GetBytes(str);
-                _ = await client.SendAsync(messageBytes, SocketFlags.None);
-                // Console.WriteLine($"Socket client sent message: \"{str}");
 
-                // Receive ack.
-                var buffer = new byte[1_024];
-                var received = await client.ReceiveAsync(buffer, SocketFlags.None);
-                var response = Encoding.UTF8.GetString(buffer, 0, received);
-
-                if (response.EndsWith("end")|| str.Equals("end"))
-                {
-                    break;
-                }
-                    response = response.Remove(response.Length);
-                    Console.WriteLine(
-                        $"Socket client received acknowledgment: \"{response}");
-            }
-
+                    Console.WriteLine("preparing to the sending a file to the server...");
+                    // Open the file and read its contents
+                    string fileName="video.mp4";
+                    // string path="/Users/chiragmemriya/Desktop/testing/";
+                    byte[] fileBytes = File.ReadAllBytes(System.IO.Path.Combine("./", fileName));
+                    // Send the file to the server
+                    client.Connect(ipEndPoint);
+                    client.Send(fileBytes); 
+                    Console.WriteLine("File has been send to the server");
+            
             client.Shutdown(SocketShutdown.Both);
         }
     }
